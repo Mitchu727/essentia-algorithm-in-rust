e7e539468aa8b65e1cb366e1df7bfd0f9abe04b9
Michu M <michal.mmtk@gmail.com>
2022-05-15 21:07:02 +0200

    Add simple bindings to numpy and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,59 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
+use pyo3::{pymodule, types::PyModule, PyResult, Python, wrap_pyfunction};
+
+#[pymodule]
+fn essentia_rust(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // example using immutable borrows producing a new array
+    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
+        a * &x + &y
     }
+
+    // example using a mutable borrow to modify an array in-place
+    fn mult(a: f64, mut x: ArrayViewMutD<'_, f64>) {
+        x *= a;
+    }
+
+    fn chroma_cross_similarity(mut x: ArrayViewD<'_, f64>, mut y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
+        4.0 * &x
+    }
+
+    // wrapper of `chroma_cross_similarity`
+    #[pyfn(m)]
+    #[pyo3(name = "ChromaCrossSimilarity")]
+    fn chroma_cross_similarity_py<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArrayDyn<f64>,
+        y: PyReadonlyArrayDyn<f64>,
+    ) -> &'py PyArrayDyn<f64> {
+        let x = x.as_array();
+        let y = y.as_array();
+        let z = chroma_cross_similarity(x, y);
+        z.into_pyarray(py)
+    }
+
+    // wrapper of `axpy`
+    #[pyfn(m)]
+    #[pyo3(name = "axpy")]
+    fn axpy_py<'py>(
+        py: Python<'py>,
+        a: f64,
+        x: PyReadonlyArrayDyn<f64>,
+        y: PyReadonlyArrayDyn<f64>,
+    ) -> &'py PyArrayDyn<f64> {
+        let x = x.as_array();
+        let y = y.as_array();
+        let z = axpy(a, x, y);
+        z.into_pyarray(py)
+    }
+
+    // wrapper of `mult`
+    #[pyfn(m)]
+    #[pyo3(name = "mult")]
+    fn mult_py(_py: Python<'_>, a: f64, x: &PyArrayDyn<f64>) {
+        let x = unsafe { x.as_array_mut() };
+        mult(a, x);
+    }
+
+    Ok(())
 }
-
-
